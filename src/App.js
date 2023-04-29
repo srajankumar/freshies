ab48d3a59c31711456f1a5caadfbeb23b043d180
srajankumar <kumarsrajan02@gmail.com>
2023-04-29 09:14:27 +0530

    more-clear-now

--- src/App.js
+++ src/App.js
@@ -9,25 +9,24 @@ import ItemSelected from "./components/ItemSelected";
 const App = () => {
   return (
     <AppProvider>
-      <div className="container">
-        <h1 className="mt-3">Menuu</h1>
-        <div className="row mt-3">
-          <div className="col-sm">
-            <CartValue />
-          </div>
-        </div>
-        <h3 className="mt-3">Shopping Cart</h3>
-        <div className="row ">
-          <div className="col-sm">
+      <div>
+        <h1>Menuu</h1>
+
+        <h3>Shopping Cart</h3>
+
+        <div>
+          <div>
             <ExpenseList />
           </div>
         </div>
-        <h3 className="mt-3">Add Items</h3>
-        <div className="row mt-3">
-          <div className="col-sm">
-            <ItemSelected />
+
+        <div>
+          <div>
+            <CartValue />
           </div>
         </div>
+
+        <h3>Thanks For Shopping??</h3>
       </div>
     </AppProvider>
   );
--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -7,12 +7,42 @@ const CartValue = () => {
     return (total += item.unitprice * item.quantity);
   }, 0);
 
+  const filteredExpenses = expenses.filter((item) => item.quantity > 0);
+
   return (
-    <div className="alert alert-primary">
-      <span>
-        Cart Value: {Currency}
-        {totalExpenses}
-      </span>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Item Name</th>
+            <th>Quantity</th>
+            <th>Unit Price</th>
+            <th>Total Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredExpenses.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+              <td>
+                {Currency}
+                {item.unitprice}
+              </td>
+              <td>
+                {Currency}
+                {item.unitprice * item.quantity}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="alert alert-primary">
+        <span>
+          Cart Value: {Currency}
+          {totalExpenses}
+        </span>
+      </div>
     </div>
   );
 };
